e5eeacccdb6fb47b94efd51e5bcc44ee5226b2a8
tamco61 <42232243+tamco61@users.noreply.github.com>
2020-03-22 20:42:19 +0500

    Add files via upload

---
+++ Пейзажи.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/carousel')
+def sample_file_upload():
+        return '''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <style type="text/css">
+                            h1
+                            {
+                                padding-left: 40%; 
+                            }
+                            img
+                            {
+                                max-width: 1000px;
+                                max-height: 500px;
+                            }
+                            div.ew
+                            {
+                                max-width: 1000px;
+                                max-height: 500px;
+                                margin: 0 auto;
+                            }
+                            </style>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                             <link rel="stylesheet"
+                             href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+                             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+                             crossorigin="anonymous">
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                            <h1>Пейзажи Марса</h1>
+                            <div class="ew">
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                              <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                              </ol>
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img class="d-block w-100" src="static/img/image.jpg" alt="Первый слайд">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="static/img/image1.jpg" alt="Второй слайд">
+                                </div>
+                                <div class="carousel-item">
+                                  <img class="d-block w-100" src="static/img/image2.jpg" alt="Третий слайд">
+                                </div>
+                              </div>
+                              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                              </a>
+                              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                              </a>
+                            </div>
+                            </div>
+                          </body>
+                        </html>'''
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
